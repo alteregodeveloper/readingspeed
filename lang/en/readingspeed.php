cfa18423587e899b50ea8377ae4408b4756ef91a
alterego developer <alteregodeveloper@outlook.com>
2019-09-25 15:40:38 -0500

    Language files are loaded

---
+++ lang/en/readingspeed.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @package   mod_readingspeed
+ * @copyright 2019, alterego developer {@link https://alteregodeveloper.github.io}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Reading Speed Test';
+$string['modulename'] = 'Reading Speed Test';
+$string['readingspeed'] = 'Reading Speed Test';
+$string['mod_readingspeed'] = 'Reading Speed Test';
+$string['readingspeedname'] = 'Name';
+$string['modulename_help'] = 'This module allows to exercise the student\'s reading speed and quantify it in three possible levels:
+* easy
+* middle
+* advanced';
+$string['category'] = 'Category';
+$string['complexity'] = 'Complexity';
+$string['easy'] = 'Easy';
+$string['medium'] = 'Middle';
+$string['advanced'] = 'Advanced';
+ 
---
+++ lang/es/readingspeed.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @package   mod_readingspeed
+ * @copyright 2019, alterego developer {@link https://alteregodeveloper.github.io}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Test de velocidad lectora';
+$string['modulename'] = 'Test de velocidad lectora';
+$string['readingspeed'] = 'Test de velocidad lectora';
+$string['mod_readingspeed'] = 'Test de velocidad lectora';
+$string['readingspeedname'] = 'Nombre';
+$string['modulename_help'] = 'Éste módulo permite ejercitar la velocidad lectora del estudiante y cuantificarlo en tres posibles niveles:
+* fácil
+* intermedio
+* avanzado';
+$string['category'] = 'Categoria';
+$string['complexity'] = 'Complejidad';
+$string['easy'] = 'Fácil';
+$string['medium'] = 'Intermedio';
+$string['advanced'] = 'Avanzado';
