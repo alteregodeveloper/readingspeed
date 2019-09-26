ec07b7efee38114413779420f7ee7c950584ed41
alterego developer <alteregodeveloper@outlook.com>
2019-09-26 18:57:31 -0500

    Initial setting is declared

---
+++ settings.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @package   mod_observationtest
+ * @copyright 2019, alterego developer {@link https://alteregodeveloper.github.io}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+if ($ADMIN->fulltree) {
+    require_once(__DIR__.'/lib.php');
+
+}
