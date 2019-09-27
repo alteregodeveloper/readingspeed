b92f9871371ab92e1e6ef32f1293a6fe4c6a919d
alterego developer <alteregodeveloper@outlook.com>
2019-09-26 19:16:18 -0500

    Functions for get categories and complexity ranges are added

---
+++ pix/locallib.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @package   mod_readingspeed
+ * @copyright 2019, alterego developer {@link https://alteregodeveloper.github.io}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+function get_complexity_ranges(){
+    return array(
+    6 => get_string('easy','readingspeed'),
+    4 => get_string('medium','readingspeed'),
+    2 => get_string('advanced','readingspeed'));
+}
+
+function get_readingspeed_categories() {
+    global $DB;
+
+    $query = 'SELECT id, category FROM mdl_reading_categories';
+    return $DB->get_records_sql_menu($query);
+}
