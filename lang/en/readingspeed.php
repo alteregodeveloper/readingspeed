d47bc7c64f1bd4a01955122977e4e39f2674cbac
alterego developer <alteregodeveloper@outlook.com>
2019-09-26 19:11:53 -0500

    Rfeading speed test form is structured

--- lang/en/readingspeed.php
+++ lang/en/readingspeed.php
@@ -11,6 +11,7 @@ $string['modulename'] = 'Reading Speed Test';
 $string['readingspeed'] = 'Reading Speed Test';
 $string['mod_readingspeed'] = 'Reading Speed Test';
 $string['readingspeedname'] = 'Name';
+$string['readingspeedwords'] = 'Words';
 $string['modulename_help'] = 'This module allows to exercise the student\'s reading speed and quantify it in three possible levels:
 * easy
 * middle
---
+++ mod_form.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * @package   mod_readingspeed
+ * @copyright 2019, alterego developer {@link https://alteregodeveloper.github.io}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+if (!defined('MOODLE_INTERNAL')) {
+    die('Direct access to this script is forbidden.');    ///  It must be included from a Moodle page
+}
+ 
+require_once($CFG->dirroot.'/course/moodleform_mod.php');
+require_once($CFG->dirroot.'/mod/readingspeed/lib.php');
+require_once($CFG->dirroot.'/mod/readingspeed/locallib.php');
+ 
+class mod_readingspeed_mod_form extends moodleform_mod {
+ 
+    function definition() {
+        global $CFG, $DB, $OUTPUT;
+ 
+        $mform =& $this->_form;
+
+        $mform->addElement('text', 'name', get_string('readingspeedname', 'readingspeed'), array('size'=>'64'));
+        $mform->setType('name', PARAM_TEXT);
+        $mform->addRule('name', null, 'required', null, 'client');
+ 
+        $categories = get_readingspeed_categories();
+        $mform->addElement('select', 'category', get_string('category', 'readingspeed'), $categories);
+        $mform->setDefault('category', 1);
+
+        $complexityranges = get_complexity_ranges();
+        $mform->addElement('select', 'complexity', get_string('complexity', 'readingspeed'), $complexityranges);
+        $mform->setDefault('complexity', 1);
+
+        $mform->addElement('text', 'words', get_string('readingspeedwords', 'readingspeed'), array('size'=>'10'));
+        $mform->setType('words', PARAM_TEXT);
+        $mform->addRule('words', null, 'required', null, 'client');
+        
+        $this->standard_intro_elements();
+        $element = $mform->getElement('introeditor');
+        $attributes = $element->getAttributes();
+        $attributes['rows'] = 10;
+        $element->setAttributes($attributes);   
+ 
+        $this->standard_coursemodule_elements();
+ 
+        $this->add_action_buttons();
+    }
+}
